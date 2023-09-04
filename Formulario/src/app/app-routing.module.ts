c233a9911b22817b63e10f65a7c8d572bd9ba1dc
koikera <legendar7gamer@hotmail.com>
2023-09-04 11:00:36 -0300

    Criacao do formulario

--- Formulario/src/app/app-routing.module.ts
+++ Formulario/src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormularioComponent } from './components/formulario/formulario.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path: '', component: FormularioComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
